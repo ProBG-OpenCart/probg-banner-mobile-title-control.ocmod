3b03715faa9770e08875f413dcca8048e3312c60
Veselin Totev <v.totev@probg.com>
2026-08-14 07:29:42 +0300

    Add English module language

---
+++ upload/admin/language/en-gb/extension/module/probg_banner_mobile.php
@@ -0,0 +1,10 @@
+<?php
+// Heading
+$_['heading_title'] = 'ProBG Banner Mobile & Title Control';
+
+// Text
+$_['text_extension'] = 'Extensions';
+$_['text_home'] = 'Home';
+$_['text_info'] = 'This extension enhances the standard OpenCart Banner functionality. Configure mobile images and title visibility in Design > Banners. Configure desktop and mobile image dimensions in each standard Banner module instance.';
+$_['text_banner_link'] = 'Open Banners';
+$_['text_modules_link'] = 'Open Modules';
