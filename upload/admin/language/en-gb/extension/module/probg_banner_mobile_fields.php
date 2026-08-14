0794ce9c964cff29c7c44c70b7399a17b5d3236f
Veselin Totev <v.totev@probg.com>
2026-08-14 07:29:48 +0300

    Add English banner field language

---
+++ upload/admin/language/en-gb/extension/module/probg_banner_mobile_fields.php
@@ -0,0 +1,8 @@
+<?php
+$_['entry_mobile_image'] = 'Mobile Image';
+$_['entry_hide_title'] = 'Hide Title';
+$_['help_hide_title'] = 'When enabled, the banner title is not exposed as visible title data to storefront templates. The original title is still kept as image alt text for accessibility.';
+$_['entry_mobile_width'] = 'Mobile Width';
+$_['entry_mobile_height'] = 'Mobile Height';
+$_['error_mobile_width'] = 'Mobile width is required and must be greater than 0!';
+$_['error_mobile_height'] = 'Mobile height is required and must be greater than 0!';
